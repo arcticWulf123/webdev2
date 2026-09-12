7798651d0c65250cc79b81cb002d842a21d188d4
jade roland eduard <jaderolandeduard.pinon@lorma.edu>
2026-09-12 21:16:15 +0800

    removed comment

--- src/main/java/com/jaderoland/webdev2/ProductController.java
+++ src/main/java/com/jaderoland/webdev2/ProductController.java
@@ -7,7 +7,7 @@ import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.*;
 
 import jakarta.validation.Valid;
-//comment
+
 @Controller
 @RequestMapping("/products")
 public class ProductController {
