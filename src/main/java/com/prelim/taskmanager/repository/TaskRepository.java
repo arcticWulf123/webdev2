741bd4d685a47521318a10419883ae2c845495b3
jade roland eduard <jaderolandeduard.pinon@lorma.edu>
2026-09-20 11:44:42 +0800

    task 3 completed

--- src/main/java/com/prelim/taskmanager/repository/TaskRepository.java
+++ src/main/java/com/prelim/taskmanager/repository/TaskRepository.java
@@ -11,6 +11,13 @@ import com.prelim.taskmanager.model.Task;
 public class TaskRepository {
     private List<Task> taskList = new ArrayList<>();
 
+
+    public TaskRepository () {
+        taskList.add(new Task(taskList.size() + 1L, "Clean my room", "Room must be swept and mopped", false));
+        taskList.add(new Task(taskList.size() + 1L, "Take out the garbage", "Dispose the garbage right outside the house", true));
+        taskList.add(new Task(taskList.size() + 1L, "Feed the cats", "Feed and clean their litter", true));
+        taskList.add(new Task(taskList.size() + 1L, "Take a walk", "Take a short, 15 minute walk outside", false));
+    }
     public List<Task> findAll() {
         return taskList;
     }
